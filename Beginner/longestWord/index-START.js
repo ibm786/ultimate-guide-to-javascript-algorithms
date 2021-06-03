54849d9e45cc5fa454dd3343b2ef2e9b952f92ce
Imran Mohamed <ibm786@icloud.com>
2021-06-03 16:38:06 +0100

    longest word

--- Beginner/longestWord/index-START.js
+++ Beginner/longestWord/index-START.js
@@ -3,11 +3,17 @@ Given a string of text, write an algorithm that returns the text received in a r
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
-
-
 function longestWord(text) {
-    // Code goes here
+  let wordArray = text.split(' ');
+  let maxLength = 0;
+  let result = '';
+  for (let i = 0; i < wordArray.length; i++) {
+    if (wordArray[i].length > maxLength) {
+      maxLength = wordArray[i].length;
+      result = wordArray[i];
+    }
+  }
+  return result;
 }
 
-
-module.exports = longestWord
+module.exports = longestWord;
