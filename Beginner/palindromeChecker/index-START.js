940d72575acdc1274e1be15d657a5af660c39a42
Imran Mohamed <ibm786@icloud.com>
2021-06-03 16:16:58 +0100

    palindrome check

--- Beginner/palindromeChecker/index-START.js
+++ Beginner/palindromeChecker/index-START.js
@@ -3,13 +3,14 @@ Given a string of text, return true or false indicating whether or not the text 
 e.g palindromeChecker('racecar') // will return true
 */
 
-
-
-
 function palindromeChecker(text) {
-    v// Code goes here
+  let textLen = text.length;
+  for (let i = 0; i < textLen / 2; i++) {
+    if (text[i] !== text[textLen - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
-
-
 
 module.exports = palindromeChecker;
